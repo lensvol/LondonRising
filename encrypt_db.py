f9647c57cd112957702b5eea0f7447506c1a8751
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2018-01-14 10:00:29 +0100

    Fix bug

--- encrypt_db.py
+++ encrypt_db.py
@@ -21,7 +21,7 @@ def process_row(row, aes):
             'doc_type':       row[8]}
     try:
         if dict['json']['body']:
-            to_encrypt = bytes(dict['json']['body'], 'ascii')
+            to_encrypt = dict['json']['body']
             padlen = 16 - len(to_encrypt)%16
             to_encrypt += bytes(chr(padlen)*padlen, 'ascii')
             encrypted = aes.encrypt(to_encrypt)
