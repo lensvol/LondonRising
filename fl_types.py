2fd5343cae8ad1df2fdd301cd05163fe28bb7c8b
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2018-02-18 20:47:00 +0100

    Parsing ChildBranches

--- fl_types.py
+++ fl_types.py
@@ -1,59 +1,60 @@
 class GameObject(object):
+
+    refnames = ['QualitiesAffected', 'QualitiesRequired', 'QualitiesPossessedList', 'Enhancements', 'Personae',
+                'StartingArea', 'LimitedToArea', 'Deck', 'Category', 'SettingIds', 'Shops', 'Availabilities',
+                'QualitiesAffectedOnTarget', 'QualitiesAffectedOnVictory', 'PurchaseQuality', 'Quality',
+                'ChildBranches', 'ParentEvent', 'areaid']
+
     def __init__(self, row_dict, recurse):
-        try:
-            self.id = row_dict['Id']
-            del row_dict['Id']
-        except KeyError:
-            self.id = 0
+        self.id = row_dict['Id']
+        del row_dict['Id']
         self.recursive_add = recurse
-        self.refs = self._get_refs(row_dict)
-        self.attrs = row_dict
+        self.attrs = {}
+        self.refs = []
+        self.init_attrs_and_refs(row_dict)
 
     def get_guid(self):
         return type(self).__name__ + str(self.id)
 
-    def _get_refs(self, row_dict):
-        ret = []
-        for x in self.get_ref_names():
-            try:
-                for y in self.destructure_ref(x, row_dict[x]):
-                    ret.append(y)
-                del row_dict[x]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-        return ret
-
-    def get_ref_names(self):
-        return ['QualitiesAffected', 'QualitiesRequired', 'QualitiesPossessedList', 'Enhancements', 'Personae',
-                'StartingArea', 'LimitedToArea', 'Deck', 'Category', 'SettingIds', 'Shops', 'Availabilities',
-                'QualitiesAffectedOnTarget', 'QualitiesAffectedOnVictory', 'PurchaseQuality', 'Quality']
+    def init_attrs_and_refs(self, row_dict):
+        for k in row_dict:
+            if k in self.refnames:
+                self.refs.extend(self.destructure_ref(k, row_dict[k]))
+            else:
+                self.attrs[k] = row_dict[k]
 
     def destructure_ref(self, name, ref):
-        if 'Qualities' in name or 'Quality' in name:
-            try:
-                return ('qualities' + str(x['AssociatedQuality']['Id']) for x in ref)
-            except (TypeError, KeyError):
-                return 'qualities' + str(ref['Id'])
+        if 'Qualities' in name or name == 'Enhancements':
+            return ('qualities' + str(x['AssociatedQuality']['Id']) for x in ref)
+        if 'Quality' in name:
+            return 'qualities' + str(ref['Id'])
+        if name == 'areaid':
+            return 'areas' + ref
         if name == 'LimitedToArea':
             return 'areas' + str(ref['Id']),
+        if 'Event' in name:
+            return 'events' + str(ref['Id']),
         if name == 'SettingIds':
             return ('settings' + str(x) for x in ref)
         if name == 'StartingArea':
             ref['type'] = 'areas'
             return self.recursive_add(ref),
-        if name == 'Personae' or name == 'Shops' or name == 'Availabilities':
+        if name == 'Personae' or name == 'Shops' or name == 'Availabilities' or name == 'ChildBranches':
             ret = []
             for x in ref:
                 x['type'] = name.lower()
                 ret.append(self.recursive_add(x))
             return ret
-        if name == 'Enhancements' or name == 'Deck':
+        if name == 'Deck':
+            if len(ref) == 0:
+                return name.lower() + str(ref)
             ref['type'] = name.lower()
             return self.recursive_add(ref),
         if name == 'Category':
-            return name.lower() + ref['Id'] + ref['Title']
+            try:
+                return name.lower() + ref['Id']
+            except TypeError:
+                return name.lower() + str(ref)
         raise ValueError("Cannot destructure reference to " + name)
 
     def to_graph_node(self):
@@ -68,10 +69,10 @@ TYPES = {typename: type(typename, (GameObject,), {}) for typename in ('sidebarco
                                                                       'accesscodes', 'newsitems', 'areas',
                                                                       'domiciles', 'events', 'deck', 'storeitems',
                                                                       'exchanges', 'shops', 'availabilities',
-                                                                      'acts')}
+                                                                      'acts', 'childbranches')}
 
 
 def parse_dict_to_game_object(row_dict, recurse):
     if 'type' not in row_dict:
         return None, None
-    return TYPES[row_dict['tye']](row_dict, recurse).to_graph_node()
+    return TYPES[row_dict['type']](row_dict, recurse).to_graph_node()
